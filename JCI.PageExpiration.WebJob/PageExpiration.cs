f1bafac9785e43bc63697328908b578af7b78813
karthikpandiyan <karthikpandiyan@hotmail.com>
2015-03-04 11:20:03 +0530

    4

---
+++ JCI.PageExpiration.WebJob/PageExpiration.cs
@@ -0,0 +1,18 @@
+﻿#region Namespace
+using JCI.PageExpiration.Data;
+using System;
+#endregion
+
+namespace JCI.PageExpiration.WebJob
+{
+    class PageExpirationJob
+    {
+        static void Main(string[] args)
+        {
+            DataAccess dataAccess = new DataAccess();
+            // Calls Root function
+            dataAccess.pageExpirationWebJob();
+
+        }
+    }
+}
